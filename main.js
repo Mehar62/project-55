9be6916a67ebbf88cc3e4d1da4c955e7ae95c96a
Mehar62 <78727941+Mehar62@users.noreply.github.com>
2021-06-10 16:01:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function one(){
+    alert("The renowned Taj Mahal was brought to life by the vision of the Mughal Emperor Shah Jahan. He got this monument built for his beloved wife, Mumtaz Mahal after she passed away. To honor the memory of his loving wife, Shah Jahan ordered the finest artisans from all over the world to build it.");
+}
+
+function two(){
+    alert("The Colosseum is an amphitheatre built in Rome under the Flavian emperors of the Roman Empire. It is also called the Flavian Amphitheatre. It is an elliptical structure made of stone, concrete, and tuff, and it stands four stories tall at its highest point.");
+}
+
+function three(){
+    alert("Chichén Itzá, ruined ancient Maya city occupying an area of 4 square miles (10 square km) in south-central Yucatán state, Mexico. It is thought to have been a religious, military, political, and commercial centre that at its peak would have been home to 35,000 people.");
+}
+
+function four(){
+    alert("Machu Picchu is a 15th-century Inca site located on a ridge between the Huayna Picchu and Machu Picchu mountains in Peru. It sits 7,970 feet (2,430 meters) above sea level on the eastern slope of the Andes and overlooks the Urubamba River hundreds of feet below.");
+}
+
+function five(){
+    alert("Petra is half-built, half-carved into the rock, and is surrounded by mountains riddled with passages and gorges. ... It is one of the world's most famous archaeological sites, where ancient Eastern traditions blend with Hellenistic architecture.");
+}
+
+function six(){
+    alert("The wall is made of cement, rocks, bricks, and dirt. It was finished in 1878 and it was meant to protect the north of the empire of China from enemy attacks. It is the longest structure humans have ever built. It is about 21,196 kilometers long, 9.1 metres (30 feet) wide and 15 metres high.");
+}
+
+function seven(){
+    alert("Christ the Redeemer is the largest art deco statue in the world. It is 98 feet tall (not including the 26 foot pedestal), and the arms stretch to 92 feet wide. The statue weighs approximately 635 tonnes. Christ the Redeemer is located in the Tijuca Forest National Forest, at the top of the Corcovado Mountain.");
+}
